961f27b2e6dd1146e2e207b7506101ef345f0dde
hnoson <hiroki015@gmail.com>
2018-08-01 00:53:02 +0900

    Add libc6-dbg for 32bit and r2dec

--- setup.sh
+++ setup.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-sudo apt install -y vim-gnome python python-pip zsh tmux ruby xclip auditd curl
+sudo apt install -y vim-gnome python python-pip zsh tmux ruby xclip auditd curl libc6:i386 libc6-dbg:i386
 chsh -s /bin/zsh
 sudo pip install pwntools
-cp .vimrc .zshrc .tmux.conf ~
+cp .vimrc .zshrc .tmux.conf .gdbinit ~
 mkdir ~/programs
 cd ~/programs
 
 # gdb
 git clone https://github.com/longld/peda.git
 git clone https://github.com/hnoson/gdbheap.git
-echo 'source ~/programs/peda/peda.py\nsource ~/programs/gdbheap/gdbheap.py' >> ~/.gdbinit
 
 # libc-database (sometimes fails)
 git clone https://github.com/niklasb/libc-database.git
@@ -30,3 +29,5 @@ sudo gem install one_gadget
 # radare2
 git clone https://github.com/radare/radare2.git
 radare2/sys/install.sh
+r2pm init
+r2pm install r2dec
