a34a1001acf05037831f80587ad28440606e1df6
hnoson <hiroki015@gmail.com>
2018-09-08 09:32:09 +0000

    Add gdb-multiarch and peda-arm

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt install -y vim-gnome python python-pip zsh tmux ruby xclip auditd curl socat pkg-config hexdiff cmake qemu-user-static gcc-arm-linux-gnueabihf
+sudo apt install -y vim-gnome python python-pip zsh tmux ruby xclip auditd curl socat pkg-config hexdiff cmake qemu-user-static gcc-arm-linux-gnueabihf gdb-multiarch
 sudo apt install -y libc6:i386 libc6-dbg:i386
 chsh -s /bin/zsh
 sudo pip install pwntools
@@ -16,6 +16,7 @@ git clone https://github.com/pwndbg/pwndbg
     cd pwndbg
     ./setup.sh
 )
+git clone https://github.com/alset0326/peda-arm.git
 
 # rp++
 mkdir rp++
