89d748a824dbb61914a10be6f39db6ff3119cfbc
hnoson <hiroki015@gmail.com>
2018-09-08 09:20:07 +0000

    Add qemu

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt install -y vim-gnome python python-pip zsh tmux ruby xclip auditd curl socat pkg-config hexdiff cmake
+sudo apt install -y vim-gnome python python-pip zsh tmux ruby xclip auditd curl socat pkg-config hexdiff cmake qemu-user-static gcc-arm-linux-gnueabihf
 sudo apt install -y libc6:i386 libc6-dbg:i386
 chsh -s /bin/zsh
 sudo pip install pwntools
@@ -28,8 +28,6 @@ sudo gem install one_gadget
 # radare2
 git clone https://github.com/radare/radare2.git
 radare2/sys/install.sh
-r2pm init
-r2pm install r2dec
 
 # libc-database
 git clone https://github.com/niklasb/libc-database.git
